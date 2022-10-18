a1bff4a7d0a44d1f6ffbc1defcb0459e4aadbe15
Mao <lcdsmao@gmail.com>
2022-10-18 17:28:01 +0900

    feat: export monkey

--- lib/monkey.dart
+++ lib/monkey.dart
@@ -1,7 +1,7 @@
-library monkey;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/monkey.dart';
+export 'src/monkey_event.dart';
+export 'src/monkey_event/fling_from_event.dart';
+export 'src/monkey_event/pop_event.dart';
+export 'src/monkey_event/tap_at_event.dart';
+export 'src/monkey_source.dart';
+export 'src/monkey_source/monkey_source_random.dart';
