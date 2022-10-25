6f3b5677971411d6a6e1027870c00949872d94be
Mao <lcdsmao@gmail.com>
2022-10-25 10:57:46 +0900

    feat: monkey context

---
+++ lib/src/monkey_context.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import 'element.dart';
+import 'random.dart';
+
+abstract class MonkeyContext {
+  Element get rootElement;
+
+  Iterable<Element> get allElements;
+
+  Element? randomElement({bool Function(Element)? test});
+
+  bool hitTestable(Element element, {Alignment alignment});
+}
+
+class MonkeyEventImpl extends MonkeyContext {
+  MonkeyEventImpl(this._binding);
+
+  final WidgetsBinding _binding;
+
+  @override
+  Element get rootElement => _binding.renderViewElement!;
+
+  @override
+  Iterable<Element> get allElements =>
+      CachingIterable(_DepthFirstChildIterator(rootElement));
+
+  @override
+  Element? randomElement({bool Function(Element element)? test}) {
+    int i = 0;
+    Element? target;
+    for (final element in allElements) {
+      if (test == null || test(element)) {
+        i++;
+        if (random.nextInt(i) == 0) {
+          target = element;
+        }
+      }
+    }
+    return target;
+  }
+
+  @override
+  bool hitTestable(
+    Element element, {
+    Alignment alignment = Alignment.center,
+  }) {
+    final box = getElementRenderBox(element);
+    if (box == null) return false;
+    final absoluteOffset = box.localToGlobal(alignment.alongSize(box.size));
+    final hitResult = HitTestResult();
+    _binding.hitTest(hitResult, absoluteOffset);
+    for (final HitTestEntry entry in hitResult.path) {
+      if (entry.target == element.renderObject) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+class _DepthFirstChildIterator implements Iterator<Element> {
+  _DepthFirstChildIterator(Element rootElement) {
+    _fillChildren(rootElement);
+  }
+
+  late Element _current;
+
+  final List<Element> _stack = <Element>[];
+
+  @override
+  Element get current => _current;
+
+  @override
+  bool moveNext() {
+    if (_stack.isEmpty) {
+      return false;
+    }
+
+    _current = _stack.removeLast();
+    _fillChildren(_current);
+
+    return true;
+  }
+
+  void _fillChildren(Element element) {
+    element.debugVisitOnstageChildren(_stack.add);
+  }
+}
